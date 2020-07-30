b99e39ca4e0b137d8f4795c1c9ebd9d1987f6c83
TheBadListener <thebadlistener@protonmail.com>
2020-07-30 01:12:51 -0400

    Added StartCaptivity Hotfix for 1.4.3

---
+++ Patches/CEPatchPlayerCaptivity.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+
+namespace CaptivityEvents.Patches
+{
+    [HarmonyPatch(typeof(PlayerCaptivity))]
+    internal class CEPatchPlayerCaptivity
+    {
+
+        [HarmonyPatch("StartCaptivity")]
+        [HarmonyPostfix]
+        private static void StartCaptivity(PartyBase captorParty)
+        {
+            // 1.4.3 Fix
+            if (PlayerEncounter.Current != null)
+            {
+                PlayerEncounter.LeaveEncounter = false;
+            }
+        }
+    }
+}
