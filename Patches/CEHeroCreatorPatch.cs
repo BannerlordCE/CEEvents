11002787f153471b27c1a33f0989436d754f51a9
DMRTYR <120136261+DMRTYR@users.noreply.github.com>
2023-03-01 22:02:08 -0800

    MCM + Female-Female Integration (#78)
    * PregnancyToggleFemalexFemale
    
    This inserts the "PregnancyToggleFemalexFemale" boolean into the custom settings
    
    * Updated CESettings to include PregnancyToggleFemalexFemale
    
    Adds the boolean PregnancyToggleFemalexFemale to the MCM to control behavior
    
    * Implemented Gendercheck Toggle
    
    Inserts a check at the point that filters out females as "senders"/"captors" that checks if they are female and if female-female impregnation is allowed
    
    * Fix incorrect parameter
    
    The "forcepreg" parameter in ImpregnationChance currently does not allow for a selected father and defaults to a random one. By switching it to false, the pregnancy is no longer guaranteed but you can control the father
    
    * Created Harmony Patch for HeroCreator
    
    This is a simple patch that modifies the result of the base game "DeliverOffSpring(...)" method so that the clan of the child will depend on the mother's clan, allowing bastard children and preventing children teleporting across the map once being born. At this time, it does not have MCM integration.
    
    * Compile Error Fixes
    
    I was finally able to get Visual Studio up so I can compile .dll files to test my edits (after a million dependencies were added to the solution)
    
    This was able to compile and run in the latest version of Bannerlord and I tested the functionality of the console commands. It is not guaranteed to work each time you run it due to how the ImpregnationChance() method works. I set Pregnancy to 100% chance for testing purposes

---
+++ Patches/CEHeroCreatorPatch.cs
@@ -0,0 +1,22 @@
+using System;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.ObjectSystem;
+
+namespace CaptivityEvents.Patches
+{
+	[HarmonyPatch(typeof(HeroCreator), "DeliverOffSpring")]
+	internal static class CEHeroCreatorPatch
+	{
+		[HarmonyPostfix]
+		public static void DeliverOffSpring(ref Hero __result, Hero mother, Hero father, bool isOffspringFemale, CultureObject culture = null)
+		{
+			__result.Culture = __result.Father.Culture;
+			__result.Clan = __result.Mother.Clan;
+			if (__result.Clan != Clan.PlayerClan)
+			{
+				__result.HasMet = false;
+			}
+		}
+	}
+}
