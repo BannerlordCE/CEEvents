313d3af2ffa97bf9200eae7dd4f37cee255658ef
TheBadListener <thebadlistener@protonmail.com>
2023-03-02 01:15:30 -0500

    Fixed of Postfix

--- Patches/CEHeroCreatorPatch.cs
+++ Patches/CEHeroCreatorPatch.cs
@@ -9,13 +9,13 @@ namespace CaptivityEvents.Patches
 	internal static class CEHeroCreatorPatch
 	{
 		[HarmonyPostfix]
-		public static void DeliverOffSpring(ref Hero __result, Hero mother, Hero father, bool isOffspringFemale, CultureObject culture = null)
+		public static void DeliverOffSpring(ref Hero __result, Hero mother, Hero father, bool isOffspringFemale)
 		{
 			__result.Culture = __result.Father.Culture;
 			__result.Clan = __result.Mother.Clan;
-			if (__result.Clan != Clan.PlayerClan)
+			if (__result.Clan == Clan.PlayerClan)
 			{
-				__result.HasMet = false;
+				__result.SetHasMet();
 			}
 		}
 	}
